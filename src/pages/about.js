550933bfd9f2c8be7acf952b403dbc09e9c87717
Jean-Elie Lecuy <nirusan@MacBook-Pro-2.local>
2018-10-06 23:31:07 +0200

    commit test

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="About GatsbyJS" />
+    <Header headerText="It's pretty cool" />
+    <p>Such wow. Very React.</p>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,22 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
+
 const IndexPage = () => (
   <Layout>
     <h1>Style Décor</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/about/">Go to about page</Link>
   </Layout>
 )
 
-export default IndexPage
+export default () => (
+  <div style={{ color: `purple` }}>
+  <h1>Hello Gatsby!</h1>
+  <p>What a world.</p>
+  <img src="https://source.unsplash.com/random/400x200" alt="" />
+  <Link to="/about/">Go to about page</Link>
+  <Link to="/contact/">Contact</Link>
+  </div>
+)
